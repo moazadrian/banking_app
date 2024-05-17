282c139719af56abec51dcff46b138a7ca96afe0
Moaz Adrian <moazadrian@gmail.com>
2024-05-18 02:02:33 +0500

    AlphaA3

--- components/ui/TotalBalanceBox.tsx
+++ components/ui/TotalBalanceBox.tsx
@@ -7,7 +7,7 @@ const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}:TotlaB
   return (
     <section className='total-balance'>
         <div className="total-balance-chart">
-            <DoughnutChart />
+            <DoughnutChart accounts={accounts}/>
         </div>
         <div className="flex flex-col gap-6">
             <h2 className="header-2">
