ffe871ca809b87aa925c5a5ab4c1a422e27ec1a0
Moaz Adrian <moazadrian@gmail.com>
2024-05-18 03:38:05 +0500

    Responsive functionality for sidebar/nav

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ({user}: SiderbarProps) => {
     return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
-            <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+            <Link href='/' className='flex mb-12 cursor-pointer items-center gap-2'>
                 <Image 
                     src='/icons/logo.svg'
                     width={34}
@@ -31,11 +31,29 @@ const Sidebar = ({user}: SiderbarProps) => {
                     <Link href={item.route} key={item.label}
                     className={cn('sidebar-link', {'bg-bank-gradient':isActive})}
                     >
-                        {item.label}
+                        <div className="relative size-6">
+                            <Image 
+                            src={item.imgURL}
+                            alt={item.label}
+                            fill
+                            className={cn({
+                                'brightness-[3] invert-0': isActive
+                            })}
+                            />
+                        </div>
+                        <p className={cn('sidebar-label', {
+                            '!text-white': isActive
+                        })}>
+                            {item.label}
+                        </p>
                         </Link>
                 )
             })}
+
+            USER
         </nav>
+
+        FOOTER
     </section>
   )
 }
