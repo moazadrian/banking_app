f12bc5ff79cba2f45da048f25d3dd9b6a84357de
Moaz Adrian <moazadrian@gmail.com>
2024-05-18 02:40:21 +0500

    Impletent functionality which Active Sidebar links

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,17 @@
+import Sidebar from "@/components/ui/Sidebar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Moaz', lastName: 'Adrian'};
+
+
   return (
-    <main>
-     SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+     <Sidebar user={loggedIn}/>
      {children}   
     </main>
   );
